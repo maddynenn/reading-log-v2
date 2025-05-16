4acea19b20a0a99e851b13be285d62bdb2d49205
maddynenn <maddynenninger@gmail.com>
2025-05-16 17:53:07 -0400

    added a login component to the landing page, which in theory would redirect you to home page if successful

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -1,7 +1,10 @@
+import { Login } from "../src/components/Login";
+
 export function Landing() {
 	return (
 		<>
 			<h1>Landing</h1>
+			<Login />
 		</>
 	);
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { verifyUser } from "../api";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export function Login() {
+	const [user, setUser] = useState({
+		email: "",
+		password: "",
+	});
+
+	const navigate = useNavigate();
+
+	function handleChange(e) {
+		setUser({
+			...user,
+			[e.target.name]: [e.target.value],
+		});
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		let response = await verifyUser(user);
+
+		if (response) {
+			sessionStorage.setItem("User", response);
+			axios.defaults.headers.common["Authorization"] = `Bearer ${response}`;
+			navigate("/home");
+		} else {
+			alert("login failed");
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<TextField
+				id="email-field"
+				label="Email"
+				variant="outlined"
+				sx={{ margin: "5px" }}
+				onChange={handleChange}
+				value={user.email}
+				required
+			/>
+			<TextField
+				id="password-field"
+				label="Password"
+				variant="outlined"
+				sx={{ margin: "5px" }}
+				onChange={handleChange}
+				value={user.password}
+				required
+			/>
+			<Button color="success" variant="outlined" type="submit">
+				Submit
+			</Button>
+		</form>
+	);
+}
