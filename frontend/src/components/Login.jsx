869188635392e382c1c7a5efe578c33e9846a758
maddynenn <maddynenninger@gmail.com>
2025-05-16 17:56:41 -0400

    put that login component in a mui Box

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { verifyUser } from "../api";
 import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
 
 export function Login() {
 	const [user, setUser] = useState({
@@ -34,28 +35,30 @@ export function Login() {
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<TextField
-				id="email-field"
-				label="Email"
-				variant="outlined"
-				sx={{ margin: "5px" }}
-				onChange={handleChange}
-				value={user.email}
-				required
-			/>
-			<TextField
-				id="password-field"
-				label="Password"
-				variant="outlined"
-				sx={{ margin: "5px" }}
-				onChange={handleChange}
-				value={user.password}
-				required
-			/>
-			<Button color="success" variant="outlined" type="submit">
-				Submit
-			</Button>
-		</form>
+		<Box display={"flex"} flexDirection={"row"}>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					id="email-field"
+					label="Email"
+					variant="outlined"
+					sx={{ margin: "5px" }}
+					onChange={handleChange}
+					value={user.email}
+					required
+				/>
+				<TextField
+					id="password-field"
+					label="Password"
+					variant="outlined"
+					sx={{ margin: "5px" }}
+					onChange={handleChange}
+					value={user.password}
+					required
+				/>
+				<Button color="success" variant="outlined" type="submit">
+					Submit
+				</Button>
+			</form>
+		</Box>
 	);
 }
