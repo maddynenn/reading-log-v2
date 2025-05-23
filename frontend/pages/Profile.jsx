f92c51fe007fe176f1564add779cc352c543382c
maddynenn <maddynenninger@gmail.com>
2025-05-23 08:27:42 -0700

    made a profile page with user's specific book entries

--- frontend/pages/Profile.jsx
+++ frontend/pages/Profile.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import * as jwt_decode from "jwt-decode";
+import { getAllBookEntries } from "../src/api";
+
 export function Profile() {
-    return (
-        <>
-            <h1>Profile</h1>
-        </>
-    )
+	const [user, setUser] = new useState({});
+	const [myBooks, setMyBooks] = new useState([]);
+
+	useEffect(() => {
+		async function loadAllUserData() {
+			const token = sessionStorage.getItem("User");
+			const decodedUser = jwt_decode.jwtDecode(token);
+			const allEntries = await getAllBookEntries();
+			const filterEntries = allEntries.filter((entry) => entry.user == decodedUser._id);
+			setMyBooks(filterEntries);
+			setUser(decodedUser);
+			console.log(myBooks);
+			console.log(decodedUser);
+		}
+		loadAllUserData();
+	}, []);
+
+	return (
+		<>
+			<h1>Profile</h1>
+			<h2>My book logs</h2>
+			<div>
+				{myBooks.map((entry) => {
+					return <p>{entry.title}</p>;
+				})}
+			</div>
+		</>
+	);
 }
