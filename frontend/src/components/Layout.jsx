f90063968fef0ed862c0b931e9ef69f79e536849
maddynenn <maddynenninger@gmail.com>
2025-05-23 11:11:39 -0400

    navigate back to landing page when not logged in

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Layout() {
+	const user = sessionStorage.getItem("User");
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!user) {
+			navigate("/");
+		}
+	}, [user]);
+
 	return (
 		<>
 			<NavBar />
