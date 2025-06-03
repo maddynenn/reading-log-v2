3e05925178add90fd1a0b7f46871351e8b98f11a
maddynenn <maddynenninger@gmail.com>
2025-06-02 21:10:24 -0400

    setup the basic home page (very basic, but has books loaded)

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,7 +1,52 @@
+import Box from "@mui/material/Box";
+import { InfoCard } from "../src/components/InfoCard";
+import { calculateBooksReadThisMonth } from "../src/utils";
+import { useState, useEffect } from "react";
+import * as jwt_decode from "jwt-decode";
+import { getAllBookEntries } from "../src/api";
 export function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		async function loadAllBookEntries() {
+			const token = sessionStorage.getItem("User");
+			const decodedUser = jwt_decode.jwtDecode(token);
+			const data = await getAllBookEntries();
+			const filteredData = data.filter((entry) => entry.user === decodedUser._id);
+			setBooks(filteredData);
+			console.log(filteredData);
+		}
+
+		loadAllBookEntries();
+	}, []);
+
+	return (
+		<>
+			<h1>Home</h1>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "flex-start",
+					bgcolor: "yellow",
+					width: "100%",
+				}}
+			>
+				<Box
+					sx={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						width: "100%",
+					}}
+				>
+					<InfoCard
+						title="Books Read This Month"
+						value={calculateBooksReadThisMonth(books)}
+						subtitle="books"
+					/>
+				</Box>
+			</Box>
+		</>
+	);
 }
