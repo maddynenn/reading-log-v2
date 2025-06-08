b3198125c6a612f3ddb3aa8d6ed42a54140bb66e
maddynenn <maddynenninger@gmail.com>
2025-06-07 23:07:29 -0400

    added more authentication stuff to backend routes

--- backend/bookEntryRoutes.js
+++ backend/bookEntryRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const database = require("./connect");
 const ObjectId = require("mongodb").ObjectId;
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./config.env" });
 
 let bookEntryRoutes = express.Router();
 
@@ -13,7 +15,7 @@ let bookEntryRoutes = express.Router();
 
 // 1 Retrieve All
 //http://localhost:8000/bookEntries
-bookEntryRoutes.route("/bookEntries").get(async (request, response) => {
+bookEntryRoutes.route("/bookEntries").get(verifyToken, async (request, response) => {
 	let db = database.getDb();
 	let data = await db.collection("bookEntry").find({}).toArray();
 	if (data.length > 0) {
@@ -24,7 +26,7 @@ bookEntryRoutes.route("/bookEntries").get(async (request, response) => {
 });
 
 // 2 Retrieve One
-bookEntryRoutes.route("/bookEntries/:id").get(async (request, response) => {
+bookEntryRoutes.route("/bookEntries/:id").get(verifyToken, async (request, response) => {
 	let db = database.getDb();
 	let data = await db.collection("bookEntry").findOne({ _id: new ObjectId(request.params.id) });
 	if (Object.keys(data).length > 0) {
@@ -35,7 +37,7 @@ bookEntryRoutes.route("/bookEntries/:id").get(async (request, response) => {
 });
 
 // 3 Create One
-bookEntryRoutes.route("/bookEntries").post(async (request, response) => {
+bookEntryRoutes.route("/bookEntries").post(verifyToken, async (request, response) => {
 	let db = database.getDb();
 	let mongoObject = {
 		title: request.body.title,
@@ -63,7 +65,7 @@ bookEntryRoutes.route("/bookEntries").post(async (request, response) => {
 });
 
 // 4 Update One
-bookEntryRoutes.route("/bookEntries/:id").put(async (request, response) => {
+bookEntryRoutes.route("/bookEntries/:id").put(verifyToken, async (request, response) => {
 	let db = database.getDb();
 	let mongoObject = {
 		$set: {
@@ -94,12 +96,30 @@ bookEntryRoutes.route("/bookEntries/:id").put(async (request, response) => {
 });
 
 // Delete One
-bookEntryRoutes.route("/bookEntries/:id").delete(async (request, response) => {
+bookEntryRoutes.route("/bookEntries/:id").delete(verifyToken, async (request, response) => {
 	let db = database.getDb();
 
 	let data = await db.collection("bookEntry").deleteOne({ _id: new ObjectId(request.params.id) });
 
 	response.json(data);
 });
+
+function verifyToken(request, response, next) {
+	const authHeaders = request.headers["authorization"];
+	const token = authHeaders && authHeaders.split(" ")[1];
+	if (!token) {
+		return response.status(401).json({ message: "Authentication token is missing" });
+	}
+
+	jwt.verify(token, process.env.SECRET_KEY, (error, user) => {
+		if (error) {
+			return response.status(403).json({ message: "Token is invalid" });
+		}
+
+		if (!request.body) request.body = {};
+		request.body.user = user;
+		next();
+	});
+}
 
 module.exports = bookEntryRoutes;
